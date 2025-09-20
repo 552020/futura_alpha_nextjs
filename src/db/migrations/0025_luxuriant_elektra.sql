9f396be765441acd79457803a629a122357dc1ae
lmangall <l.mangallon@gmail.com>
2025-09-20 11:17:27 +0200

    chore: delete migrations

--- src/db/migrations/0025_luxuriant_elektra.sql
+++
@@ -1,5 +0,0 @@
-ALTER TYPE "public"."storage_pref_t" ADD VALUE 's3';--> statement-breakpoint
-ALTER TYPE "public"."storage_pref_t" ADD VALUE 'vercel-blob';--> statement-breakpoint
-ALTER TABLE "document" ALTER COLUMN "metadata" SET DEFAULT '{"size":0,"mimeType":"","originalName":"","uploadedAt":"2025-09-15T18:22:20.082Z"}'::json;--> statement-breakpoint
-ALTER TABLE "image" ALTER COLUMN "metadata" SET DEFAULT '{"size":0,"mimeType":"","originalName":"","uploadedAt":"2025-09-15T18:22:20.080Z"}'::json;--> statement-breakpoint
-ALTER TABLE "video" ALTER COLUMN "metadata" SET DEFAULT '{"size":0,"mimeType":"","originalName":"","uploadedAt":"2025-09-15T18:22:20.082Z"}'::json;
